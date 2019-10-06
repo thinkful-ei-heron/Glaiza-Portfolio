fe3a65531bf51062c15bfb63a6b990145c143266
Glaiza Wagner <glaizawagner@gmail.com>
2019-10-06 09:35:51 -0700

    partial
    html wireframe done
    css - still need to update media queries

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// // function showNavigationList() {
+// //   $('.navigation').on('click', 'li' function(event) {
+// //       event.preventDefault();
+// //       const showList = $(event.currentTarget);
+// //       console.log('on click is working');
+// //     $('.navigation').close();
+// //     $('#section-about').show(showList);
+// //    // render();
+// //   });
+// // }
+
+// function closeMenu() {
+//     $('.navigation__link').on('click', 'li', function() {
+//       $('.menulinks').toggleClass('hidden');
+//     });
+//   }
+
+//   function bindEventListener() {
+//     closeMenu()
+//   }
+
+// $(bindEventListener);
